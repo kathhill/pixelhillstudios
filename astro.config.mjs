1e4e05bb8365fc736bd98a2dccf14d20ec4a8532
kathhill <kath.hill@sbcglobal.net>
2024-11-21 12:45:50 -0800

    feat: Add sitemap xml, clean-up files, do final tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [astroImageTools],    
+  site: "https://pixelhillstudios.com",
+  integrations: [astroImageTools, sitemap()],
 });
