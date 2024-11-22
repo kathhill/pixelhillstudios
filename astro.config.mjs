d56b7dad629e252e1d9e16c6df220c3a3b589e78
kathhill <kath.hill@sbcglobal.net>
2024-11-22 14:06:28 -0800

    feat: Integrate Partytown for Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://pixelhillstudios.com",
-  integrations: [astroImageTools, sitemap()],
+  integrations: [
+    astroImageTools,
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
